c7552d98ffb5507633e453b4c76c1d2fb57980d3
Noah Turner <noahkt32@outlook.com>
2017-08-28 12:00:31 -0600

    Classes

---
+++ 1600Fall2017/Assets/Fish.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Animal {
+
+    void Swim () {
+        print(this.name + " Swims");
+    }
+
+    void Start()
+    {
+        Swim ();
+    }
+
+}
