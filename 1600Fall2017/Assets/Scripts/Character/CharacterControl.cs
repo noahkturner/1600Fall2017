8b93b9c732f104abe3707a6934c6647ead8cde9c
Noah Turner <noahkt32@outlook.com>
2017-10-16 12:00:03 -0600

    dafsdaewafews fwesdeafsdavfsdafv

--- 1600Fall2017/Assets/Scripts/Character/CharacterControl.cs
+++ 1600Fall2017/Assets/Scripts/Character/CharacterControl.cs
@@ -9,21 +9,14 @@ public class CharacterControl : MonoBehaviour {
 	public float speed = 10;
 	public Vector3 moveVector3;
 	public float jumpForce = 20;
-
-	void Start () {
-		
-	}
-	
 	void FixedUpdate () {
 		moveVector3.y -= gravity * Time.deltaTime;
-		
 		if (characterController.isGrounded) 
 		{
 			if (Input.GetKey(KeyCode.Space))
 			{
 				moveVector3.y += jumpForce * Time.deltaTime;
 			}
-			
 			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		}
 		characterController.Move(moveVector3);
