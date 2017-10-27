13ee1153079383303f839d2b3518a71386ebac2b
Noah Turner <noahkt32@outlook.com>
2017-10-27 11:54:02 -0600

    i thiknk so

--- 1600Fall2017/Assets/Scripts/Character/CharacterControl.cs
+++ 1600Fall2017/Assets/Scripts/Character/CharacterControl.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour {
 
 	public CharacterController characterController;
+	public static bool gameOver = false;
 	public float gravity = 9.81f;
 	public float speed = 10;
 	public Vector3 moveVector3;
 	public float jumpForce = 20;
 	void Update () {
 		moveVector3.y -= gravity * Time.deltaTime;
-		if (characterController.isGrounded) 
+		if (characterController.isGrounded && !gameOver) 
 		{
 			if (Input.GetKeyDown(KeyCode.Space))
 			{
