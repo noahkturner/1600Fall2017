2359bf06c60421356900d8c3bbc463dc27529248
Noah Turner <noahkt32@outlook.com>
2017-11-10 11:44:49 -0700

    my teacher is radicallll

---
+++ 1600Fall2017/Assets/Scripts/New Folder/Laser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	public Transform start;
+	public Transform end;
+	public LineRenderer line;
+
+	// Update is called once per frame
+	void Update () {
+		line.SetPosition(0, start.position);
+		line.SetPosition(1, end.position);
+	}
+}
