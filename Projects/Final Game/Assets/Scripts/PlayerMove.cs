1229cbb7674af590712fe8467f656d3cded46de6
Noah Turner <noahkt32@outlook.com>
2017-11-15 00:32:02 -0700

    fromthe ground up

---
+++ Projects/Final Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float speed = 15;
+	public float gravity = 9.81f;
+	public CharacterController playerMove;
+	public Vector3 movement;
+	public float jumpHeight = 12;
+	public static bool gameState = false;
+	
+	void Update () {
+		movement.y -= gravity * Time.deltaTime;
+		if (playerMove.isGrounded && !gameState)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				movement.y = jumpHeight * Time.deltaTime;
+			}
+			movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		}
+		playerMove.Move(movement);
+	}
+}
