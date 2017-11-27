5955fa29eb8eb06fc7c32cd772b7cac3af78aa9b
Noah Turner <noahkt32@outlook.com>
2017-11-27 08:53:11 -0700

    idungedit

--- Projects/Final Game/Assets/Scripts/PlayGame.cs
+++ Projects/Final Game/Assets/Scripts/PlayGame.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayGame : MonoBehaviour {
 
 	public static Vector3 start;
 	public GameObject gameStart;
 	public Transform player;
+	public Image lifeBar;
+	private float fillBar;
 
 	
 	void Awake () {
 		gameStart.SetActive(false);
 		start = player.position;
+		fillBar = lifeBar.fillAmount;
 	}
 	
 	
 	public void Click () {
 		PlayerMove.gameState = false;
 		gameStart.SetActive(false);
+		lifeBar.fillAmount = fillBar;
+		player.position = startPosition;
 	}
 }
